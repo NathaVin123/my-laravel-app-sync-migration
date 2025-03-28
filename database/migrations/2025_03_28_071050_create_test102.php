67c72e42133d03caf614e5a21ecc95438eb58d33
NathaVin123 <nathanielvincentwibowo@gmail.com>
2025-03-28 07:13:43 +0000

    Merge database/migrations dari master ke dev

--- database/migrations/2025_03_28_071050_create_test102.php
+++ database/migrations/2025_03_28_071050_create_test102.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('test102', function (Blueprint $table) {
             $table->id();
-            $table->id();
             $table->timestamps();
         });
     }
